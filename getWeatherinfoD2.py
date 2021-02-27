8e5497296f5ef7710568186b4db8d8594a4f65a0
yuuKiwAw <zhangtuo1999@gmail.com>
2021-02-27 14:45:53 +0800

    add csweather

--- getWeatherinfoD2.py
+++ getWeatherinfoD2.py
@@ -1,15 +1,17 @@
 '''
 Author       : Yuki
 Date         : 2021-02-24 19:45:49
-LastEditors  : Yuki
-LastEditTime : 2021-02-26 23:29:29
-FilePath     : \suzhou_hjzl\getWeatherinfoD2.py
+LastEditors: Please set LastEditors
+LastEditTime: 2021-02-27 14:34:58
+FilePath     : \\suzhou_hjzl\\getWeatherinfoD2.py
 '''
+# python 3.8
 # coding=utf-8
 import requests
 import json
 import pandas as pd
-import time
+import datetime
+import saveToFile
 from lxml import etree
 from lxml import html
 
@@ -37,7 +39,15 @@ def getJsonVal(strFile, keyName):
     # keyValue = dict_data.get(keyName)  # 获取键值
 
 
-# 获取request返回信息
+# 生成前一天的日期文本（xxxx年xx月xx日）
+def yesterdayStr():
+    today = datetime.date.today()  # !本机当天时间（确保运行的终端时间正确）
+    oneday = datetime.timedelta(days=1)  # 一天的时间
+    yesterday = today - oneday  # 获取到昨天的日期
+    return yesterday.strftime('%Y年%m月%d日')  # 返回经过格式化的值
+
+
+# 获取request返回信息(给爬虫方法提供)
 def getReq(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -56,10 +66,10 @@ def getReq(url):
 
 
 # etee Xpath爬取常熟市的AQI和污染源信息
-def getinfoFromXpath():
+def CSRBinfo():
     """
     Returns:
-        [dict]: [aqi and wry values]
+        [dict]: [aqi and key values]
     """
     url_getCityDaily = "http://sthjj.suzhou.gov.cn//szhbj/kqzlrb/airshow.shtml"
     reponseHtml = getReq(url_getCityDaily)
@@ -73,15 +83,13 @@ def getinfoFromXpath():
     CSAQI = AQIresultList[0]
     CSAQI = str(CSAQI).split('：')  # 字符切片并取值
     CSSYWRW = WRWresultList[5]
-    CSinfo = {'CSAQI': CSAQI[1], 'CSSYWRW': CSSYWRW}
+    CSDATE = yesterdayStr()
+    CSinfo = {'CSDATE': CSDATE, 'CSAQI': CSAQI[1], 'CSSYWRW': CSSYWRW}
     return CSinfo
 
 
-# 主函数
-def main():
-    print("执行时间：" + time.asctime(time.localtime(time.time())))
-
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+def SZHJJinfo():
+    # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     # 苏州市空气质量实时报
     url_getCityHourAQI = "http://sthjj.suzhou.gov.cn/consultfront/consult/getCityHourAQI"  # 空气质量实时报request URL
     CityHourvalue = getReq(url_getCityHourAQI)
@@ -92,13 +100,38 @@ def main():
     AQINUM = getJsonVal(CityHourvalue, "AQI")  # AQI指数
     SYWRW = getJsonVal(CityHourvalue, "SYWLW")  # 首污染物
     NONGDU = getJsonVal(CityHourvalue, "nongdu")  # 浓度
-    print("苏州市空气质量实时报：", getTime, airLevel, airClass, AQINUM, SYWRW, NONGDU)
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    # 常熟市空气质量日报
-    print("常熟市的AQI指数和首要污染物：")
-    changshuInfoDaily = getinfoFromXpath()  # !返回的字典值
-    print(changshuInfoDaily)
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    # print("苏州市空气质量实时报：")
+    SZHOURINFO = {'SZRETIME': getTime, 'SZREAL': airLevel, 'SZREAC': airClass, 'SZREAQI': AQINUM, 'SZREWRW': SYWRW, 'SZREND': NONGDU}  # !返回的字典值
+    # print(SZHOURINFO)
+    return SZHOURINFO
+
+
+def RTCSweather():
+    # API接口（GET方式）// 天气API网站说明：https://www.tianqiapi.com/index/doc?version=v6
+    # 常熟市实时天气
+    # !每天免费仅限300次调用
+    url_api = "https://tianqiapi.com/api?version=v6&appid=57527268&appsecret=NnpIc7MS&city=常熟"
+    responsejsonVal = requests.get(url_api)
+    if(responsejsonVal.status_code == 200):
+        jsonresult = responsejsonVal.text
+        json2 = json.loads(jsonresult)  # 字典格式
+        return json2  # !返回的字典值
+    else:
+        print("调用天气API出错")
+
+
+# 主函数
+def main():
+    print("执行时间：" + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))  # 本机当前时间
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>苏州空气质量实时报告>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(SZHJJinfo())  # 苏州空气质量实时报告
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>常熟空气质量日报>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(CSRBinfo())  # 常熟空气质量日报
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>常熟实时天气预报>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(RTCSweather())  # 常熟实时天气预报
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    NEWDICTTOFILE = saveToFile.DICTTOFILE('./csv/data.csv', SZHJJinfo(), CSRBinfo(), RTCSweather())  # 实例化一个新的类
+    NEWDICTTOFILE.dict_to_csv()  # 调用类中的函数转换为csv
 
 
 if __name__ == '__main__':
